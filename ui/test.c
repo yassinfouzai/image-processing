b0ce0635164da544b308925024c2fbcc2fbf6acd
yassinfouzai <yassinfouzai258@gmail.com>
2024-12-20 19:04:55 +0100

    added a button and a background

--- ui/test.c
+++ ui/test.c
@@ -8,6 +8,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Button structure
+typedef struct {
+    SDL_Rect rect;
+    SDL_Color color;
+    SDL_Color hoverColor;
+    SDL_Color clickColor;
+    bool isHovered;
+    bool isClicked;
+} Button;
 
 bool init() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -76,6 +85,22 @@ bool loadPNG(const char *filename,SDL_Renderer *renderer, SDL_Texture **texture)
     return true;
 }
 
+// Function to render a button
+void renderButton(SDL_Renderer *renderer, Button *button) {
+    if (button->isClicked) {
+        SDL_SetRenderDrawColor(renderer, button->clickColor.r, button->clickColor.g, button->clickColor.b, 255);
+    } else if (button->isHovered) {
+        SDL_SetRenderDrawColor(renderer, button->hoverColor.r, button->hoverColor.g, button->hoverColor.b, 255);
+    } else {
+        SDL_SetRenderDrawColor(renderer, button->color.r, button->color.g, button->color.b, 255);
+    }
+    SDL_RenderFillRect(renderer, &button->rect);
+}
+
+// Function to check if a point is inside a rectangle
+bool isPointInsideRect(int x, int y, SDL_Rect *rect) {
+    return x > rect->x && x < rect->x + rect->w && y > rect->y && y < rect->y + rect->h;
+}
 
 int main(int argc, char* argv[])
 {
@@ -88,17 +113,55 @@ int main(int argc, char* argv[])
     if(!createRender(win,&renderer)) goto cleanup;
     if(!loadPNG("file.png",renderer,&texture)) goto cleanup;
     
+    // Create a button
+    Button button = {
+        .rect = {350, 250, 100, 50}, // x, y, width, height
+        .color = {0, 128, 255, 255}, // Default color (blue)
+        .hoverColor = {0, 200, 255, 255}, // Hover color (light blue)
+        .clickColor = {0, 100, 200, 255}, // Click color (dark blue)
+        .isHovered = false,
+        .isClicked = false
+    };
 
+    // Define a destination rectangle for the image (scaled down)
+    SDL_Rect dstRect = {100, 100, 400, 300};  // Position (100, 100) with size (400, 300)
+    
     bool quit = false;
     SDL_Event event;
     while(!quit){
         while(SDL_PollEvent(&event)){
-            if (event.type == SDL_QUIT)
-                quit = true;
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_MOUSEMOTION:
+                    // Check if mouse is over the button
+                    button.isHovered = isPointInsideRect(event.motion.x, event.motion.y, &button.rect);
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    // Check if button is clicked
+                    if (button.isHovered && event.button.button == SDL_BUTTON_LEFT) {
+                        button.isClicked = true;
+                    }
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    // Reset button click state
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        button.isClicked = false;
+                    }
+                    break;
+            }
+
         }
 
+        // Set the background color (e.g., light gray: RGB(200, 200, 200))
+        SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+        // Render the button
+        renderButton(renderer, &button);
         SDL_RenderPresent(renderer);
     }
     
